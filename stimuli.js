9b84d3df9f2b82daba95376bb0e7c73c2701ccd8
Yubin Zhang <yubinzha@usc.edu>
2026-08-30 23:52:05 -0500

    edit files

---
+++ stimuli.js
@@ -0,0 +1,13 @@
+// 注意：变量名必须叫 stimuliData，不要修改
+const stimuliData = [
+    { s: "སྲུང་ངོས་འདི་ཨ་མི་རི་ཀ་ནི་སློབ་དཔོན་གྲགས་ཅན་གྱིས་བྲིས་བཞུགས།", t: "This novel is written by famous american writer" },
+    { s: "གཙང་ཆུ་འདི་གྲུ་ཆུང་འདི་བཟུམ་གྱི་དོན་ལུ་གཏིང་ལངས་མ་སྟེ་མིན་འདུག", t: "This river isn't deep enough for this kind of boat." },
+    { s: "སློབ་དུས་འདི་ནང་གི་སློབ་ཕྲུག་གཉིས་དཔེ་ཆ་ལས་བརྟེན་མཐར་མ་འགྱོལ།", t: "This semester I failed two students for plagiarism." },
+    { s: "ཞབས་ཁྲོ་འདི་གིས་ང་གི་འབྲིང་རིམ་སློབ་གྲྭ་བར་མའི་དུས་ཚོད་དྲན་གསོ་འབདཝ་མས།", t: "This song reminds me of my junior high school days." },
+    { s: "ཤིང་འདི་ནགས་ཚལ་འདི་ནང་རིང་ཤོས་དང་རྒས་ཤོས་ཅིག་ཨིན་པས།", t: "This tree is the tallest and oldest in this forest." },
+    { s: "མི་སྟོང་ཕྲག་ལས་བཅས་ནད་གཞི་འདི་གི་གནོད་པ་ཕོག་ཡི།", t: "Thousands of people became victims of this disease." },
+    { s: "ཕྱི་རྒྱལ་གྱི་སྐད་སློབ་ནི་གི་དོན་ལས་དུས་ཚོད་རེ་བ་དགོཔ་ལས།", t: "To learn a foreign language requires a lot of time." },
+    { s: "བདེན་པ་བཤད་ནི་གི་དོན་ལུ་ ང་གི་ཁོ་གི་སློབ་སྟངས་འདི་མི་དགའ་བས།", t: "To tell the truth, I don't like his way of talking." },
+    { s: "ང་གི་ཤེས་པ་དང་འཁྲིལ་བ་ཅིན་ འཛོར་གཏམ་འདི་མི་བདེན།", t: "To the best of my knowledge, the rumor is not true." },
+    { s: "ཏཕས་གཙུག་ལག་སློབ་འདི་ བོས་ཊོན་ལུ་སློབ་གྲྭ་སྐད་གྲགས་ཅན་ཅིག་ཨིན།", t: "Tufts University is a very famous school in Boston." }
+];
